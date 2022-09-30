97074eca8ef1632c16ff1c9b3bf0e57d89ffea09
Jean-Cairoli <jcairoli1993@gmail.com>
2022-09-30 10:01:04 -0600

    improved App test file

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,20 +1,10 @@
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
-import Header from '../components/Header';
 import CardContainer from '../components/CardContainer';
 import JSONDATA from './MOCKDATA.json';
 
 describe('App test', () => {
-	it('should display the <Header /> component', () => {
-		render(<Header />);
-		const header = screen.getByRole('heading', {
-			name: /fandom wiki/i,
-		});
-		expect(header).toBeInTheDocument();
-		expect(header).toHaveTextContent(/fandom wiki/i);
-	});
-
 	it('should display <SearchBar /> component', () => {
 		render(<App />);
 		const searchBar = screen.getByRole('textbox', {
