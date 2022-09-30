e8d9f7b4258b9fd735a1df5039dea53cf3a1060b
Jean-Cairoli <jcairoli1993@gmail.com>
2022-09-30 09:05:04 -0600

    improved header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,33 @@
 import { Box, Typography } from '@mui/material';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import './Header.css';
 
 const Header = () => {
 	return (
-		<Box className="container container-header backgund">
-			<Typography variant="h1">FANDOM WIKI</Typography>
+		<Box className="header">
+			<Box className="social-icons">
+				<a
+					target="_blank"
+					rel="noreferrer"
+					href="https://github.com/Jean-Cairoli/Puppies-App"
+					className="redirect"
+				>
+					<GitHubIcon fontSize="large" />
+				</a>
+				<a
+					target="_blank"
+					rel="noreferrer"
+					href="https://www.linkedin.com/feed/"
+					className="redirect"
+				>
+					<LinkedInIcon fontSize="large" />
+				</a>
+			</Box>
+
+			<Typography variant="h5" className="name">
+				Jean Cairoli
+			</Typography>
 		</Box>
 	);
 };
